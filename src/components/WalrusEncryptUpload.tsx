b1e7fcd8c59b3eb78bb2c35732a23b75704cae7f
juSt-jeLLy <yagneshpatel931@gmail.com>
2025-11-18 22:42:30 +0530

    wallrus

--- src/components/WalrusEncryptUpload.tsx
+++ src/components/WalrusEncryptUpload.tsx
@@ -22,7 +22,7 @@ type DatasetInfo = {
   txDigest: string;
 };
 
-const PACKAGE_ID = "0xb39bacd6af7db0417da87846d032573a315ac309e32aca72c743e2e2444e80d7"; // Package ID with publish function for WalrusBlob attachment
+const PACKAGE_ID = "0xb486f1a7bcca26a704f93e07439ea61d7f04f5855eaea850e40b5371d0b1a6b5"; // Package ID with publish function for WalrusBlob attachment
 const SERVER_OBJECT_IDS = [
   "0x73d05d62c18d9374e3ea529e8e0ed6161da1a141a94d3f76ae3fe4e99356db75",
   "0xf5d14a81a982144ae441cd7d64b09027f116a468bd36e7eca494f750591623c8"
